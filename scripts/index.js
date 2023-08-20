f7f782f772d584799bb8f7085cd93256d13c5b46
mrana007 <rana.es.bc@gmail.com>
2023-08-20 21:24:03 +0600

    added discount and final total and modal

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,34 @@ function handleClick(target){
     selectedItemContainer.appendChild(li);
     const price = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
     totalPrice = parseFloat(totalPrice) + parseFloat(price);
-    document.getElementById('total-price').innerText = totalPrice;
-    const total = document.getElementById('total').innerText = totalPrice;
-    return total;
+    document.getElementById('total-price').innerText = totalPrice.toFixed(2);
+    document.getElementById('total').innerText = totalPrice.toFixed(2);
+    const purchaseButton = document.getElementById('purchase-button');
+    if (totalPrice > 0) {
+        purchaseButton.removeAttribute("disabled");
+    }
+    else {
+        purchaseButton.setAttribute("disabled", "disabled");
+    }
+    const applyButton = document.getElementById('apply-button');
+    if (totalPrice >= 200) {
+        applyButton.removeAttribute("disabled");
+    }
+    else {
+        applyButton.setAttribute("disabled", "disabled");
+    }
+    document.getElementById('apply-button').addEventListener('click', applyCoupon);
+    function applyCoupon() {
+        const couponInput = document.getElementById('coupon-code');
+        const couponCode = couponInput.value.trim();
+    
+        if (couponCode === 'SELL200' && totalPrice >= 200) {
+            discount = totalPrice * 0.2;
+            document.getElementById('discount-amount').innerText = discount.toFixed(2);
+            finalTotal();
+        }
+        else{
+            alert('Please insert valid coupon');
+        }
+    }
 }
