9482250f0017d66a2241d4e98dfb306af024316c
mrana007 <rana.es.bc@gmail.com>
2023-08-20 23:15:01 +0600

    overlook all html and js file

--- scripts/index.js
+++ scripts/index.js
@@ -11,25 +11,25 @@ function handleClick(target){
     totalPrice = parseFloat(totalPrice) + parseFloat(price);
     document.getElementById('total-price').innerText = totalPrice.toFixed(2);
     document.getElementById('total').innerText = totalPrice.toFixed(2);
+
     const purchaseButton = document.getElementById('purchase-button');
-    
     if (totalPrice > 0) {
         purchaseButton.removeAttribute('disabled');
     }
     else {
-        purchaseButton.setAttribute('disabled', 'disabled');
+        purchaseButton.setAttribute('disabled');
     }
     const applyButton = document.getElementById('apply-button');
     if (totalPrice >= 200) {
         applyButton.removeAttribute('disabled');
     }
     else {
-        applyButton.setAttribute('disabled', 'disabled');
+        applyButton.setAttribute('disabled');
     }
     document.getElementById('apply-button').addEventListener('click', applyCoupon);
     function applyCoupon() {
         const couponInput = document.getElementById('coupon-code');
-        const couponCode = couponInput.value.trim();
+        const couponCode = couponInput.value;
     
         if (couponCode === 'SELL200' && totalPrice >= 200) {
             discount = totalPrice * 0.2;
@@ -45,7 +45,7 @@ function handleClick(target){
         const total = document.getElementById('total');
         total.innerText = totalWithDiscount.toFixed(2);
     }
-    document.getElementById('go-home').addEventListener('click', function(){
-        window.location.href = 'index.html';
-    })
 }
+document.getElementById('go-home').addEventListener('click', function(){
+    window.location.href = 'index.html';
+})
